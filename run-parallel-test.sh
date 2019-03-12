c6a6fef7b31dca6f277007910d4700bc88758f1d
Stefan Lay <stefan.lay@sap.com>
2019-03-12 13:26:13 +0100

    Use simple go test app

--- run-parallel-test.sh
+++ run-parallel-test.sh
@@ -15,10 +15,9 @@ rm -rf gen/parallel
 mkdir -p gen/parallel
 
 for i in `seq 1 50`; do
-  echo "https://x-forwarded-for.cf.$DOMAIN/" >> $FILE
-  echo "https://api.cf.$DOMAIN/v2/info/" >> $FILE
+  echo "https://hello-go-test.cfapps.$DOMAIN/" >> "$FILE"
 done
 
 while true; do
-  cat "$FILE" | parallel "curl -sSi -X POST -d @data-1MB.file {}" >> "gen/parallel/$DOMAIN.log" 2>> "gen/parallel/$DOMAIN.err"
+  parallel "curl -sSi" >> "gen/parallel/$DOMAIN.log" 2>> "gen/parallel/$DOMAIN.err" < "$FILE"
 done
